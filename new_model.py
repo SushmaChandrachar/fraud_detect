2d2e09ba6fb99d78a9fbea17f448a77e6f9b7afe
SushmaChandrachar <sushma.kushi@gmail.com>
2022-06-26 03:21:56 +0530

    added 3 model for fraud detect

--- new_model.py
+++ new_model.py
@@ -1,1 +1,10 @@
 print("adding all the model related contents here in new branch")
+
+def Model1:
+    return print("adding model 1")
+
+def Model2:
+    return print("adding model 2")
+
+def Model3:
+    return print("adding model 3")
